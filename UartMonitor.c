e32adb7693bde5d58fd27fc50a26238ce82f79af
Benjamin <benjamin0906@gmail.com>
2021-01-03 17:48:13 +0100

    Opening serial ports have been added.

--- UartMonitor.c
+++ UartMonitor.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
 
 int main(void)
 {
+    int SerialPortOne = serialOpen("/dev/ttyS0", 9600);
+    int SerialPortTwo = serialOpen("/dev/ttyAMA1", 9600);
+    if((SerialPortOne < 0) || (SerialPortTwo < 0))
+    {
+        printf("Error while opening serial port\n");
+    }
+    else
+    {
+        
+    }
     printf("Hello world");
     return 0;
 }
