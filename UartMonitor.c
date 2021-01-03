433746095fe8ec4f494ccd6764339218c171a9c8
Benjamin <benjamin0906@gmail.com>
2021-01-03 19:03:30 +0100

    Printing received characters and serial port information have been added. On every line feed it prints the serial port name too.

--- UartMonitor.c
+++ UartMonitor.c
@@ -8,12 +8,39 @@ int main(void)
     int SerialPortTwo = serialOpen("/dev/ttyAMA1", 9600);
     if((SerialPortOne < 0) || (SerialPortTwo < 0))
     {
-        printf("Error while opening serial port\n");
+        printf("Error while opening serial port: %d, %d\n", SerialPortOne<0, SerialPortTwo<0);
     }
     else
     {
-        
+        char PrintLabelOne = 1;
+        char PrintLabelTwo = 1;
+        while(1)
+        {
+            char SerialOneData;
+            char SerialTwoData;
+            if(serialDataAvail(SerialPortOne) != 0)
+            {
+                if(PrintLabelOne != 0)
+                {
+                    printf("S1: ");
+                    PrintLabelOne = 0;
+                }
+                SerialOneData = serialGetchar(SerialPortOne);
+                printf("%c",SerialOneData);
+                if(SerialOneData == '\n') PrintLabelOne = 1;
+            }
+            if(serialDataAvail(SerialPortTwo) != 0)
+            {
+                if(PrintLabelTwo != 0)
+                {
+                    printf("S2: ");
+                    PrintLabelTwo = 0;
+                }
+                SerialTwoData = serialGetchar(SerialPortTwo);
+                printf("%c", SerialTwoData);
+                if(SerialTwoData == '\n') PrintLabelTwo = 1;
+            }
+        }
     }
-    printf("Hello world");
     return 0;
 }
