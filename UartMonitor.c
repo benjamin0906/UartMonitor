cb9f39147d955ea2a50a201ac126ca3e5f6866fa
benjamin0906 <benjamin0906@gmail.com>
2021-01-03 20:04:10 +0100

    Writing out the received data properly with the information of the serial port has been implemented.

--- UartMonitor.c
+++ UartMonitor.c
@@ -2,6 +2,12 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
+/* TXD1: GPIO_14
+   RXD1: GPIO_15
+   TXD2: GPIO_0
+   RXD2: GPIO_1
+ */
+
 int main(void)
 {
     int SerialPortOne = serialOpen("/dev/ttyS0", 9600);
@@ -22,25 +28,33 @@ int main(void)
             {
                 if(PrintLabelOne != 0)
                 {
-                    printf("S1: ");
+                    printf("(S1:)");
                     PrintLabelOne = 0;
                 }
                 SerialOneData = serialGetchar(SerialPortOne);
                 printf("%c",SerialOneData);
                 fflush(stdout);
-                if(SerialOneData == '\n') PrintLabelOne = 1;
+                if(SerialOneData == '\n')
+                {
+                    PrintLabelOne = 1;
+                    PrintLabelTwo = 1;
+                }
             }
             if(serialDataAvail(SerialPortTwo) != 0)
             {
                 if(PrintLabelTwo != 0)
                 {
-                    printf("S2: ");
+                    printf("(S2:)");
                     PrintLabelTwo = 0;
                 }
                 SerialTwoData = serialGetchar(SerialPortTwo);
                 printf("%c", SerialTwoData);
                 fflush(stdout);
-                if(SerialTwoData == '\n') PrintLabelTwo = 1;
+                if(SerialTwoData == '\n')
+                {
+                    PrintLabelOne = 1;
+                    PrintLabelTwo = 1;
+                }
             }
         }
     }
